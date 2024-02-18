a5f6e44ca88ca07ba672434ae95a35dd132c4caa
Nicolas Aparicio <nicolas.aparicio23@gmail.com>
2024-02-18 11:36:30 -0300

    borrar todo lo que no es necesario

--- ejemplo/src/app/app.component.ts
+++ ejemplo/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'ejemplo-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template:
+  '<header><h1> Hola soy Bob.</h1></header>',
 })
 export class AppComponent {
-  title = 'ejemplo';
+
 }
