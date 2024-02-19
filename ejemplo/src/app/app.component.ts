e6c13ef33920f9803de0f170d1c694dd57e349d1
Nicolas Aparicio <nicolas.aparicio23@gmail.com>
2024-02-18 21:21:09 -0300

    feat:mostrar balance de sillys

--- ejemplo/src/app/app.component.ts
+++ ejemplo/src/app/app.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { computedAsync } from 'ngxtension/computed-async';
 
 @Component({
   standalone: true,
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'ejemplo-root',
   template: `
-    <header class="py-8">
-        <h1 class="text-5xl text-center mb-4"> Hola soy Bob. </h1>
+    <header class="py-8 relative">
+        <h1 class="text-5xl text-center mb-4"> Hola soy Bob </h1>
         <div class="flex justify-center">
           <hd-wallet-multi-button></hd-wallet-multi-button>
         </div>
+        
+        @if (account()){
+          <div class="absolute top-4 left-4 flex justify-center items-center gap-2">
+            <img [src]="account()?.info?.image" class="w-8 h-8" />
+
+          </div>
+        }
+
     </header>
 ,
 `
 })
 export class AppComponent {
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);  /* $ = indica que es un observable */
+
+  readonly account = computedAsync (
+    () => this._shyftApiService.getAcccount(this._publicKey()?.toBase58()), {requireSync: true},
+  );
 
 }
--- ejemplo/src/app/shyft-api.service.ts
+++ ejemplo/src/app/shyft-api.service.ts
@@ -1,10 +1,28 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { map, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root'})
 export class ShyftApiService {
-    private readonly _httpCliente: HttpClient = inject(HttpClient); 
+    private readonly _httpClient = inject(HttpClient); 
     private readonly _header = { 'x-api-key': 'CtHdU0hZAhyFAhWG' };
-    private readonly _mint  = '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs';
-    
+    private readonly _mint  = '7VcnUPSoYq9bcfCUhsxdVPdKEnkw8Rkf7Qc6BMcyg4JB';
+     /*private readonly _mint = '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs';*//*Dirección de prueba del token Silly Dragon*/
+    getAcccount(publicKey: string | undefined | null) {
+        if (!publicKey) {
+            return of(null);
+        }
+
+        const url = new URL('https://api.shyft.to/sol/v1/wallet/token_balance');
+        
+        url.searchParams.set('network', 'mainnet-beta');
+        url.searchParams.set('wallet', publicKey);
+        url.searchParams.set('token', this._mint)
+
+        return this._httpClient.get<{
+            result: {balance: number; info:{image: string}};
+        }>(url.toString(), { headers: this._header})
+        .pipe(map((response)=> response.result));
+
+    }
 }
